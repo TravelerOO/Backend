dbea16f1c60d42a148acec346db83e8076683a5c
GOYEJIN <yaejingo@gmail.com>
2023-05-09 14:12:38 +0900

    Feat :ResponseMessage 추가

--- src/main/java/com/example/miniproject/dto/http/ResponseMessage.java
+++ src/main/java/com/example/miniproject/dto/http/ResponseMessage.java
@@ -4,6 +4,8 @@ package com.example.miniproject.dto.http;
 public class ResponseMessage {
     public static final String LOGIN_SUCCESS = "로그인 성공";
     public static final String LOGIN_FAIL = "로그인 실패";
+    public static final String LOGOUT_SUCCESS = "로그아웃 성공";
+    public static final String Available_ID = "로그아웃 성공";
     public static final String READ_USER = "회원 정보 조회 성공";
     public static final String NOT_FOUND_USER = "회원을 찾을 수 없습니다.";
     public static final String CREATED_USER = "회원 가입 성공";
