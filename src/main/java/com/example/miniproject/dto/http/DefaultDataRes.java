852e4213f8c7c1f085e75079793594390227416a
Kim Seonmin <w.hand715@gmail.com>
2023-05-09 17:25:47 +0900

    Fix : CORS 이슈 관련 코드 추가

--- src/main/java/com/example/miniproject/dto/http/DefaultDataRes.java
+++ src/main/java/com/example/miniproject/dto/http/DefaultDataRes.java
@@ -15,10 +15,6 @@ public class DefaultDataRes<T> extends DefaultRes<T> {
         this.data = data;
     }
 
-    public DefaultDataRes(int statusCode, String responseMessage) {
-        super(statusCode, responseMessage);
-    }
-
     public static <T> DefaultDataRes<T> dataRes(final int statusCode, final String responseMessage, T data) {
         return (DefaultDataRes<T>) dataRes(statusCode, responseMessage, data);
     }
