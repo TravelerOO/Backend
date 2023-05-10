857fbef31469e9c2d6d2f8bedd4c75312a0f1dca
김지승 <jjk5158@naver.com>
2023-05-10 13:37:56 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,6 +3,8 @@
 ROOT_PATH="/home/ubuntu/spring-github-action"
 STOP_LOG="$ROOT_PATH/stop.log"
 
+NOW=$(date +%c)
+
 echo "[$NOW] 이전에 실행 중인 서비스를 종료합니다." >> $STOP_LOG
 SERVICE_PIDS=$(pgrep -f "java -jar")
 for SERVICE_PID in $SERVICE_PIDS; do
