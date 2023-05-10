ae99f6639cccb69f55c70517a0ae5089cbb955d4
김지승 <jjk5158@naver.com>
2023-05-10 13:09:49 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,21 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
+echo "[$NOW] 이전에 실행 중인 서비스를 종료합니다." >> $START_LOG
+SERVICE_PIDS=$(pgrep -f "java -jar")
+for SERVICE_PID in $SERVICE_PIDS; do
+  SERVICE_JAR=$(readlink -f /proc/$SERVICE_PID/exe | cut -d' ' -f1-)
+  if [[ "$SERVICE_JAR" =~ .*\.jar$ ]]; then
+    kill $SERVICE_PID
+    echo "[$NOW] > 종료한 서비스: $SERVICE_JAR, PID: $SERVICE_PID" >> $START_LOG
+  fi
+done
+
+echo "[$NOW] $JAR 파일을 복사합니다." >> $START_LOG
 cp $ROOT_PATH/build/libs/*.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
+echo "[$NOW] $JAR 파일을 실행합니다." >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+echo "[$NOW] > 새로운 서비스 PID: $SERVICE_PID" >> $START_LOG
