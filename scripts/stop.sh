0ebdbd2f5f342d11d09f49585e36abd879cb1c08
김지승 <jjk5158@naver.com>
2023-05-10 21:53:07 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,7 +11,7 @@ NOW=$(date +%c)
 if [ -z "$SERVICE_PID" ]; then
   echo " [$NOW] 서비스 NouFound" >> $STOP_LOG
 else
-  echo " [$NOW] 서비스 종료 " >> $STOP_LOG
+  echo " [$NOW] [$SERVICE_PID] 서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
